6fde594d59d2a771352966df1a95c63c19307f98
rasnim <bjh713@khu.ac.kr>
2021-07-23 15:45:04 +0900

    21.07.23 update

---
+++ ch2/HelloCV/main.cpp
@@ -0,0 +1,21 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello OpenCV" << CV_VERSION << std::endl;
+	
+	cv::Mat img;
+	img = cv::imread("lenna.bmp", cv::IMREAD_UNCHANGED);
+
+	if (img.empty()) {
+		std::cerr << "Image load failed" << std::endl;
+		return -1;
+	}
+
+	cv::namedWindow("image");
+	cv::imshow("image", img);
+
+	cv::waitKey();
+	return 0;
+}
